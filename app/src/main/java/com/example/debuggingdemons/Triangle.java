85def98e140750502a0347822a7df81e0c8ee05f
Kele778 <100812664+Kele778@users.noreply.github.com>
2023-11-01 07:20:23 -0600

    Delete app directory

--- app/src/main/java/com/example/debuggingdemons/Triangle.java
+++
@@ -1,9 +0,0 @@
-package com.example.debuggingdemons;
-
-public class Triangle extends Shape{
-    private int z;
-    public Triangle(int x, int y, int z) {
-        super(x, y);
-        this.z = z;
-    }
-}
