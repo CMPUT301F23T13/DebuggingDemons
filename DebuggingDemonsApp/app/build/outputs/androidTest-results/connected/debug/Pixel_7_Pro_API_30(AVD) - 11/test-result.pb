07bf28135744f8a793dcb2d1fb8f45e0aeae8c84
JunCheng Yuan <127810357+Ayjc@users.noreply.github.com>
2023-11-09 00:03:09 -0700

    update inventory delete test

---
+++ DebuggingDemonsApp/app/build/outputs/androidTest-results/connected/debug/Pixel_7_Pro_API_30(AVD) - 11/test-result.pb
@@ -0,0 +1,20 @@
+
+ø
+s
+InventoryDeleteTestcom.example.debuggingdemonsapp testIsInventoryFragmentDisplayed2§†²ª€êî’:¨†²ª€šÛº"•
+
+logcatandroidÿ
+üC:\Users\AlexYuan\AndroidStudioProjects\DebuggingDemons\DebuggingDemonsApp\app\build\outputs\androidTest-results\connected\debug\Pixel_7_Pro_API_30(AVD) - 11\logcat-com.example.debuggingdemonsapp.InventoryDeleteTest-testIsInventoryFragmentDisplayed.txt"Ê
+
+device-infoandroid¯
+¬C:\Users\AlexYuan\AndroidStudioProjects\DebuggingDemons\DebuggingDemonsApp\app\build\outputs\androidTest-results\connected\debug\Pixel_7_Pro_API_30(AVD) - 11\device-info.pb"Ë
+
+device-info.meminfoandroid¨
+¥C:\Users\AlexYuan\AndroidStudioProjects\DebuggingDemons\DebuggingDemonsApp\app\build\outputs\androidTest-results\connected\debug\Pixel_7_Pro_API_30(AVD) - 11\meminfo"Ë
+
+device-info.cpuinfoandroid¨
+¥C:\Users\AlexYuan\AndroidStudioProjects\DebuggingDemons\DebuggingDemonsApp\app\build\outputs\androidTest-results\connected\debug\Pixel_7_Pro_API_30(AVD) - 11\cpuinfo*¯
+c
+test-results.logOcom.google.testing.platform.runtime.android.driver.AndroidInstrumentationDriver¹
+¶C:\Users\AlexYuan\AndroidStudioProjects\DebuggingDemons\DebuggingDemonsApp\app\build\outputs\androidTest-results\connected\debug\Pixel_7_Pro_API_30(AVD) - 11\testlog\test-results.log 2
+text/plain
--- DebuggingDemonsApp/app/src/androidTest/java/com/example/debuggingdemonsapp/InventoryDeleteTest.java
+++ DebuggingDemonsApp/app/src/androidTest/java/com/example/debuggingdemonsapp/InventoryDeleteTest.java
@@ -4,23 +4,12 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.view.View;
-
-import androidx.test.espresso.PerformException;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.util.HumanReadables;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,67 +28,18 @@ public class InventoryDeleteTest {
         // Navigate to inventory
         onView(withId(R.id.navigation_inventory)).perform(click());
 
-//        // Check if inventory fragment is displayed
-//        onView(withId(R.id.fragment_inventory)).check(ViewAssertions.matches(isDisplayed()));
-    }
+        onView(withId(R.id.filter_button)).check(ViewAssertions.matches(isDisplayed()));
 
-    @Test
-    public void deleteItemTest() {
-        // Navigate to inventory
-        onView(withId(R.id.navigation_inventory)).perform(click());
+        onView(withId(R.id.sortby_button)).check(ViewAssertions.matches(isDisplayed()));
 
-        // Assume there are items in the list and the first item can be deleted
-        // Click on the checkbox of the first item
-        onView(withId(R.id.recycler_view))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, clickChildViewWithId(R.id.item_checkbox)));
+        onView(withId(R.id.recycler_view)).check(ViewAssertions.matches(isDisplayed()));
 
-        // Click on the 'Delete' button
-        onView(withId(R.id.delete_button)).perform(click());
+        onView(withId(R.id.tag_button)).check(ViewAssertions.matches(isDisplayed()));
 
-        // Check if the Snackbar message is displayed
-        onView(withText("Item deleted successfully")).check(ViewAssertions.matches(isDisplayed()));
-    }
+        onView(withId(R.id.delete_button)).check(ViewAssertions.matches(isDisplayed()));
 
+        onView(withId(R.id.add_button)).check(ViewAssertions.matches(isDisplayed()));
 
-    // Helper method to perform click on child views within RecyclerView item
-    // Helper method to perform click on child views within RecyclerView item
-    public static ViewAction clickChildViewWithId(final int id) {
-        return new ViewAction() {
-            @Override
-            public Matcher<View> getConstraints() {
-                return new TypeSafeMatcher<View>() {
-                    @Override
-                    public boolean matchesSafely(View view) {
-                        // 这里可以添加更多的检查条件，比如view是否可点击等
-                        return view != null;
-                    }
-
-                    @Override
-                    public void describeTo(Description description) {
-                        description.appendText("View with id " + id + " is not found.");
-                    }
-                };
-            }
-
-            @Override
-            public String getDescription() {
-                return "Click on a child view with specified id.";
-            }
-
-            @Override
-            public void perform(UiController uiController, View view) {
-                View v = view.findViewById(id);
-                if (v != null) {
-                    v.performClick();
-                } else {
-                    throw new PerformException.Builder()
-                            .withActionDescription(this.getDescription())
-                            .withViewDescription(HumanReadables.describe(view))
-                            .withCause(new IllegalStateException("No view with ID " + id + " was found."))
-                            .build();
-                }
-            }
-        };
     }
 
 }
