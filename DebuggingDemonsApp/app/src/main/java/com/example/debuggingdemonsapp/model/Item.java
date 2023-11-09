987265424269e60a500362367f245eed08712db5
JunCheng Yuan <127810357+Ayjc@users.noreply.github.com>
2023-11-08 17:17:01 -0700

    Adding delete functionality to MergeBranch

--- DebuggingDemonsApp/app/src/main/java/com/example/debuggingdemonsapp/model/Item.java
+++ DebuggingDemonsApp/app/src/main/java/com/example/debuggingdemonsapp/model/Item.java
@@ -14,6 +14,9 @@ public class Item {
     private Drawable image2;
     private Drawable image3;
 
+    private boolean isSelected; // Used to track whether an entry is selected.
+    private String id; // Used to uniquely identify an entry in the database.
+
     // Constructors
     public Item() {}
 
@@ -99,4 +102,22 @@ public class Item {
     public Drawable getImage3() {return this.image3;}
 
     public void setImage3(Drawable image3) {this.image3 = image3;}
+
+    // Getters and Setters for the new attributes
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
