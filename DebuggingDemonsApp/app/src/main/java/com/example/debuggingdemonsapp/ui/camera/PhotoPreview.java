3584bae30f90a416de4e6142ff09ddc7e7478aa1
Kele778 <100812664+Kele778@users.noreply.github.com>
2023-11-29 21:27:10 -0700

    Readded missing changes from BarcodeScanner update

--- DebuggingDemonsApp/app/src/main/java/com/example/debuggingdemonsapp/ui/camera/PhotoPreview.java
+++ DebuggingDemonsApp/app/src/main/java/com/example/debuggingdemonsapp/ui/camera/PhotoPreview.java
@@ -1,6 +1,8 @@
 package com.example.debuggingdemonsapp.ui.camera;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.net.Uri;
@@ -11,9 +13,11 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 import com.example.debuggingdemonsapp.MainActivity;
 import com.example.debuggingdemonsapp.R;
 import com.example.debuggingdemonsapp.databinding.FragmentPictureBinding;
+import com.example.debuggingdemonsapp.ui.scanning.Scanner;
 import com.example.debuggingdemonsapp.model.Photograph;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,6 +30,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static androidx.navigation.Navigation.findNavController;
@@ -36,6 +41,8 @@ import static androidx.navigation.Navigation.findNavController;
 public class PhotoPreview extends Fragment {
 
     private FragmentPictureBinding binding;
+    private Scanner imageScanner;
+    private int rotationDegrees;
     private Photograph newPhoto;
     private FirebaseStorage storage;
 
@@ -67,6 +74,8 @@ public class PhotoPreview extends Fragment {
 
         storage = FirebaseStorage.getInstance();
 
+        imageScanner = new Scanner();
+        rotationDegrees = getArguments().getInt("Rotation");
 
 
 
@@ -81,83 +90,19 @@ public class PhotoPreview extends Fragment {
             }
         });
 
+        binding.scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Scans the image for any serial numbers/barcodes
+                imageScan();
+            }
+        });
         binding.saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getContext(), "Photo Saved", getId()).show();
-
-                // Adding images to the Firestore Storage from https://firebase.google.com/docs/storage/web/upload-files
-                // A directory is created for the current_user's photos if it doesn't already exist
-                // Photos are added with the name 'image' + the current size of the items list
-
-                // Outstanding Issue (to be fixed for project part 4): When the app closes, the appPhotos list resets but the database still has
-                //    all the photos. Thus, when this line runs it overwrites old photos. Need to have appPhotos fetch data from database.
-                StorageReference storageRef = storage.getReference(((MainActivity) getActivity()).current_user+"/image"+((MainActivity) getActivity()).appPhotos.getPhotos().size()+".jpg");
-                ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-
-                // Needed to rotate image for it to be in the right orientation in the database
-                // Need to figure out how to have image orientation based on camera/phone orientation
-                Matrix matrix = new Matrix();
-                matrix.postRotate(90);
-
-                Bitmap rotatednewPhoto = Bitmap.createBitmap(newPhoto.photoBitmap(),0,0, newPhoto.photoBitmap().getWidth(),newPhoto.photoBitmap().getHeight(),matrix,true);
-
-                rotatednewPhoto.compress(Bitmap.CompressFormat.JPEG, 100, byteOutput);
-                byte[] imageData = byteOutput.toByteArray();
-
-
-                // Upload the image data to the firebase storage associated with the current_user
-                storageRef.putBytes(imageData).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull @NotNull Exception e) {
-
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                // Sets the Uri in the newPhoto Photograph object to the Uri that firebase storage creates
-                                // This is so that it can be used later on for display images
-                                newPhoto.setUri(uri.toString());
-                            }
-                        });
-
-                    }
-                });
-
-                //
-                ((MainActivity) getActivity()).appPhotos.addPhoto(newPhoto);
-
-
-                backToCamera(container);
-                storageRef.putBytes(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                newPhoto.setUri(uri.toString());
-
-                                Map<String, Object> imageInfo = new HashMap<>();
-                                imageInfo.put("url", uri.toString());
-                                imageInfo.put("timestamp", FieldValue.serverTimestamp());
-
-                                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                                db.collection("images")
-                                        .add(imageInfo)
-                                        .addOnSuccessListener(documentReference -> {
-                                            Toast.makeText(getContext(), "Photo uploaded and saved.", Toast.LENGTH_SHORT).show();
-                                        })
-                                        .addOnFailureListener(e -> {
-                                            Toast.makeText(getContext(), "Error saving photo.", Toast.LENGTH_SHORT).show();
-                                        });
-                            }
-                        });
-                    }
-                });
+               savePhoto(container);
+               backToCamera(container);
             }
         });
 
@@ -167,7 +112,130 @@ public class PhotoPreview extends Fragment {
         return root;
     }
 
+    /**
+     * This method is used to convert the list of barcode values into a long string separated by newline characters
+     * @param barcodeList A list of the raw value(s) from the barcode(s) that were scanned
+     * @return String  A string object which contains the list of barcodes in a format that the dialog message builder can take
+     */
+    private String displayBarcodes(List<String> barcodeList){
+        StringBuilder barcodeMessage = new StringBuilder();
+        for(String value: barcodeList){
+            barcodeMessage.append("Barcode " + (barcodeList.indexOf(value)+1) +": ");
+            if (barcodeList.indexOf(value) == barcodeList.size()-1){
+                barcodeMessage.append(value);
+            }else{
+                barcodeMessage.append(value).append('\n');
+            }
 
+        }
+        return barcodeMessage.toString();
+    }
+
+    private void imageScan(){
+        MutableLiveData<List> barcodeData = imageScanner.scanImageBarcodes(newPhoto.photoBitmap(),rotationDegrees);
+
+        imageScanner.scanSerialNumber(newPhoto.photoBitmap(), rotationDegrees);
+        barcodeData.observe(getViewLifecycleOwner(), list -> {
+            if(list.isEmpty()){
+                Toast.makeText(getContext(),"No Barcode Found", Toast.LENGTH_SHORT).show();
+            }else{
+                // Add pop-up which displays barcode information
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Barcode(s) Found")
+                        .setMessage(displayBarcodes(list))
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getContext(), "Save photo to save information", Toast.LENGTH_SHORT).show();
+                                // The barcode values of the photo is set to the list of strings received from the imageScanner barcode scanner
+                                newPhoto.setBarcodeValues(barcodeData.getValue());
+
+
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        });
+    }
+    /**
+     * This method is used to save the photo when the 'save button' is pressed
+     * @param container This is a ViewGroup object that is used to
+     */
+    public void savePhoto(ViewGroup container){
+
+        // Adding images to the Firestore Storage from https://firebase.google.com/docs/storage/web/upload-files
+        // A directory is created for the current_user's photos if it doesn't already exist
+        // Photos are added with the name 'image' + the current size of the items list
+
+        // Outstanding Issue (to be fixed for project part 4): When the app closes, the appPhotos list resets but the database still has
+        //    all the photos. Thus, when this line runs it overwrites old photos. Need to have appPhotos fetch data from database.
+        StorageReference storageRef = storage.getReference(((MainActivity) getActivity()).current_user+"/image"+((MainActivity) getActivity()).appPhotos.getPhotos().size()+".jpg");
+        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+
+        // Needed to rotate image for it to be in the right orientation in the database
+        // Need to figure out how to have image orientation based on camera/phone orientation
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+
+        Bitmap rotatednewPhoto = Bitmap.createBitmap(newPhoto.photoBitmap(),0,0, newPhoto.photoBitmap().getWidth(),newPhoto.photoBitmap().getHeight(),matrix,true);
+
+        rotatednewPhoto.compress(Bitmap.CompressFormat.JPEG, 100, byteOutput);
+        byte[] imageData = byteOutput.toByteArray();
+
+
+        // Upload the image data to the firebase storage associated with the current_user
+        storageRef.putBytes(imageData).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Sets the Uri in the newPhoto Photograph object to the Uri that firebase storage creates
+                        // This is so that it can be used later on for display images
+                        newPhoto.setUri(uri.toString());
+                    }
+                });
+
+            }
+        });
+
+        //
+        ((MainActivity) getActivity()).appPhotos.addPhoto(newPhoto);
+
+
+        backToCamera(container);
+        storageRef.putBytes(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        newPhoto.setUri(uri.toString());
+
+                        Map<String, Object> imageInfo = new HashMap<>();
+                        imageInfo.put("url", uri.toString());
+                        imageInfo.put("timestamp", FieldValue.serverTimestamp());
+
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        db.collection("images")
+                                .add(imageInfo)
+                                .addOnSuccessListener(documentReference -> {
+                                    Toast.makeText(getContext(), "Photo uploaded and saved.", Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(getContext(), "Error saving photo.", Toast.LENGTH_SHORT).show();
+                                });
+                    }
+                });
+            }
+        });
+    }
         @Override
         public void onDestroyView() {
             super.onDestroyView();
